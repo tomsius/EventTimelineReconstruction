122e2ab1ea5784b83a6bdb5a07d6c170bfe04d3a
Tomas Kašelynas <tomaskas98@gmail.com>
2022-05-18 16:33:12 +0300

    Add view to import events

---
+++ EventTimelineReconstruction/EventTimelineReconstruction/Views/ImportView.xaml.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Win32;
+using System.Windows;
+
+namespace EventTimelineReconstruction.Views;
+
+/// <summary>
+/// Interaction logic for ImportView.xaml
+/// </summary>
+public partial class ImportView : Window
+{
+    public ImportView()
+    {
+        InitializeComponent();
+    }
+
+    private void ChooseFileButton_Click(object sender, RoutedEventArgs e)
+    {
+        OpenFileDialog openFileDialog = new();
+        openFileDialog.Filter = "CSV files (*.csv)|*.csv|All files (*.*)|*.*";
+
+        if (openFileDialog.ShowDialog() == true)
+        {
+            FileNameTextBox.Text = openFileDialog.FileName;
+        }
+    }
+}
