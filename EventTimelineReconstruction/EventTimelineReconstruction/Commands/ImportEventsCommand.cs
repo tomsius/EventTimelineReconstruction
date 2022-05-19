5d88edd0c740017313efb07678c4a3b90991869a
Tomas Kašelynas <tomaskas98@gmail.com>
2022-05-19 11:44:48 +0300

    Partially implement event import

---
+++ EventTimelineReconstruction/EventTimelineReconstruction/Commands/ImportEventsCommand.cs
@@ -0,0 +1,35 @@
+﻿using EventTimelineReconstruction.ViewModels;
+using System;
+using System.ComponentModel;
+
+namespace EventTimelineReconstruction.Commands;
+
+public class ImportEventsCommand : CommandBase
+{
+    private readonly ImportViewModel _importViewModel;
+
+    public ImportEventsCommand(ImportViewModel importViewModel)
+    {
+        _importViewModel = importViewModel;
+
+        _importViewModel.PropertyChanged += OnViewModelPropertyChanged;
+    }
+
+    public override bool CanExecute(object parameter)
+    {
+        return !string.IsNullOrEmpty(_importViewModel.FileName) && base.CanExecute(parameter);
+    }
+
+    public override void Execute(object parameter)
+    {
+        throw new NotImplementedException();
+    }
+
+    private void OnViewModelPropertyChanged(object sender, PropertyChangedEventArgs e)
+    {
+        if (e.PropertyName == nameof(ImportViewModel.FileName))
+        {
+            OnCanExecuteChanged();
+        }
+    }
+}
