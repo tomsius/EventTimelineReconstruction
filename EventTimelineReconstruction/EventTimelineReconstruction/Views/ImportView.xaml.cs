d8c6bc3baa544461c17075e6bbfcb8b723c10591
Tomas Kašelynas <tomaskas98@gmail.com>
2022-05-19 11:46:50 +0300

    Bind ImportView with ImportViewModel

--- EventTimelineReconstruction/EventTimelineReconstruction/Views/ImportView.xaml.cs
+++ EventTimelineReconstruction/EventTimelineReconstruction/Views/ImportView.xaml.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.Win32;
-using System.Windows;
+﻿using System.Windows;
 
 namespace EventTimelineReconstruction.Views;
 
@@ -11,16 +10,5 @@ public partial class ImportView : Window
     public ImportView()
     {
         InitializeComponent();
-    }
-
-    private void ChooseFileButton_Click(object sender, RoutedEventArgs e)
-    {
-        OpenFileDialog openFileDialog = new();
-        openFileDialog.Filter = "CSV files (*.csv)|*.csv|All files (*.*)|*.*";
-
-        if (openFileDialog.ShowDialog() == true)
-        {
-            FileNameTextBox.Text = openFileDialog.FileName;
-        }
     }
 }
