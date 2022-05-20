be22d0a968d0bdf621e8ca85689e00ee9d89078c
Tomas Kašelynas <tomaskas98@gmail.com>
2022-05-20 13:47:06 +0300

    Implement event import from log2timeline CSV file

---
+++ EventTimelineReconstruction/EventTimelineReconstruction/Services/IEventsImporter.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using EventTimelineReconstruction.Models;
+
+namespace EventTimelineReconstruction.Services;
+
+public interface IEventsImporter
+{
+    public List<EventModel> Import(string path, DateTime fromDate, DateTime toDate);
+}
---
+++ EventTimelineReconstruction/EventTimelineReconstruction/Services/L2tCSVEventsImporter.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using EventTimelineReconstruction.Models;
+
+namespace EventTimelineReconstruction.Services;
+public class L2tCSVEventsImporter : IEventsImporter
+{
+    public List<EventModel> Import(string path, DateTime fromDate, DateTime toDate)
+    {
+        List<EventModel> events = new(File.ReadLines(path).Count());
+        object lockObj = new();
+
+        Parallel.ForEach(File.ReadLines(path), (line, _, lineNumber) =>
+        {
+            if (lineNumber != 0) {
+                string[] columns = line.Split(',');
+                EventModel eventModel = ConvertRowToModel(columns);
+                DateTime eventDate = new(eventModel.Date.Year, eventModel.Date.Month, eventModel.Date.Day, eventModel.Time.Hour, eventModel.Time.Minute, eventModel.Time.Second);
+
+                if (DateTime.Compare(eventDate, fromDate) < 0 || DateTime.Compare(eventDate, toDate) > 0) {
+                    return;
+                }
+
+                lock (lockObj) {
+                    events.Add(eventModel);
+                }
+            }
+        });
+
+        Thread.Sleep(5000);
+        return events;
+    }
+
+    private static EventModel ConvertRowToModel(string[] columns)
+    {
+        DateOnly date = ConvertColumnToDate(columns[0]);
+        TimeOnly time = ConvertColumnToTime(columns[1]);
+        TimeZoneInfo timezone = TimeZoneInfo.FindSystemTimeZoneById(columns[2]);
+        string mACB = columns[3];
+        string source = columns[4];
+        string sourceType = columns[5];
+        string type = columns[6];
+        string user = columns[7];
+        string host = columns[8];
+        string shortDescription = columns[9];
+        string description = columns[10];
+        double version = double.Parse(columns[11]);
+        string filename = columns[12];
+        string iNode = columns[13];
+        string notes = columns[14];
+        string format = columns[15];
+        Dictionary<string, string> extra = ConvertColumnToExtra(columns[16]);
+
+        EventModel newEvent = new(date, time, timezone, mACB, source, sourceType, type, user, host, shortDescription, description, version, filename, iNode, notes, format, extra);
+        return newEvent;
+    }
+
+    private static DateOnly ConvertColumnToDate(string column)
+    {
+        string[] dateParts = column.Split('/');
+        int year = int.Parse(dateParts[2]);
+        int month = int.Parse(dateParts[0]);
+        int day = int.Parse(dateParts[1]);
+
+        DateOnly date = new(year, month, day);
+        return date;
+    }
+
+    private static TimeOnly ConvertColumnToTime(string column)
+    {
+        string[] timeParts = column.Split(':');
+        int hours = int.Parse(timeParts[0]);
+        int minutes = int.Parse(timeParts[1]);
+        int seconds = int.Parse(timeParts[2]);
+        TimeOnly time = new(hours, minutes, seconds);
+        return time;
+    }
+
+    private static Dictionary<string, string> ConvertColumnToExtra(string column)
+    {
+        string[] extraParts = column.Split(';');
+        Dictionary<string, string> extra = new(extraParts.Length);
+
+        foreach (string part in extraParts) {
+            string[] pair = part.Split(": ");
+            string key = pair[0];
+            string value = pair[1];
+
+            extra.Add(key, value);
+        }
+
+        return extra;
+    }
+}
