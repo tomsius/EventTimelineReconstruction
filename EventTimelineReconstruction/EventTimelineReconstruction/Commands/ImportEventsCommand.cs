328bb9ab3fe4e3aa16d30410c5b4008161f542c1
Tomas Kašelynas <tomaskas98@gmail.com>
2022-05-20 13:57:54 +0300

    Import events without locking UI thread

--- EventTimelineReconstruction/EventTimelineReconstruction/Commands/ImportEventsCommand.cs
+++ EventTimelineReconstruction/EventTimelineReconstruction/Commands/ImportEventsCommand.cs
@@ -1,18 +1,22 @@
-﻿using EventTimelineReconstruction.ViewModels;
-using System;
+﻿using EventTimelineReconstruction.Models;
+using EventTimelineReconstruction.Services;
+using EventTimelineReconstruction.ViewModels;
+using System.Collections.Generic;
 using System.ComponentModel;
+using System.Threading.Tasks;
 
 namespace EventTimelineReconstruction.Commands;
 
-public class ImportEventsCommand : CommandBase
+public class ImportEventsCommand : AsyncCommandBase
 {
     private readonly ImportViewModel _importViewModel;
+    private readonly IEventsImporter _eventsImporter;
 
-    public ImportEventsCommand(ImportViewModel importViewModel)
+    public ImportEventsCommand(ImportViewModel importViewModel, IEventsImporter eventsImporter)
     {
         _importViewModel = importViewModel;
-
-        _importViewModel.PropertyChanged += OnViewModelPropertyChanged;
+        _eventsImporter = eventsImporter;
+        _importViewModel.PropertyChanged += this.OnViewModelPropertyChanged;
     }
 
     public override bool CanExecute(object parameter)
@@ -20,16 +24,17 @@ public class ImportEventsCommand : CommandBase
         return !string.IsNullOrEmpty(_importViewModel.FileName) && base.CanExecute(parameter);
     }
 
-    public override void Execute(object parameter)
+    public override async Task ExecuteAsync(object parameter)
     {
-        throw new NotImplementedException();
+        List<EventModel> e = null;
+        await Task.Run(() => e = _eventsImporter.Import(_importViewModel.FileName, _importViewModel.FromDate, _importViewModel.ToDate));
     }
 
     private void OnViewModelPropertyChanged(object sender, PropertyChangedEventArgs e)
     {
         if (e.PropertyName == nameof(ImportViewModel.FileName))
         {
-            OnCanExecuteChanged();
+            this.OnCanExecuteChanged();
         }
     }
 }
