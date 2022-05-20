baa5bdce62a1e529fd07787cc0c538d40cccd657
Tomas Kašelynas <tomaskas98@gmail.com>
2022-05-20 13:55:54 +0300

    Add command base for async operations

---
+++ EventTimelineReconstruction/EventTimelineReconstruction/Commands/AsyncCommandBase.cs
@@ -0,0 +1,42 @@
+﻿
+using System.Threading.Tasks;
+
+namespace EventTimelineReconstruction.Commands;
+public abstract class AsyncCommandBase : CommandBase
+{
+    private bool _isExecuting;
+
+    private bool IsExecuting
+    {
+        get
+        {
+            return _isExecuting;
+        }
+        set
+        {
+            _isExecuting = value;
+            this.OnCanExecuteChanged();
+        }
+    }
+
+    public override bool CanExecute(object parameter)
+    {
+        return !IsExecuting && base.CanExecute(parameter);
+    }
+
+    public override async void Execute(object parameter)
+    {
+        IsExecuting = true;
+
+        try 
+        {
+            await this.ExecuteAsync(parameter);
+        }
+        finally
+        {
+            IsExecuting = false;
+        }
+    }
+
+    public abstract Task ExecuteAsync(object parameter);
+}
