35d3d0606d5b011a3939fdd679afe0e34a217636
Tomas Kašelynas <tomaskas98@gmail.com>
2022-05-23 12:48:50 +0300

    Add range of items to observable collection

---
+++ EventTimelineReconstruction/EventTimelineReconstruction/ViewModels/RangeEnabledObservableCollection.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+
+namespace EventTimelineReconstruction.ViewModels;
+public class RangeEnabledObservableCollection<T> : ObservableCollection<T>
+{
+    public void InsertRange(IEnumerable<T> items)
+    {
+        this.CheckReentrancy();
+
+        foreach (T item in items) 
+        { 
+            Items.Add(item);
+        }
+
+        this.OnCollectionChanged(new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Reset));
+    }
+}
