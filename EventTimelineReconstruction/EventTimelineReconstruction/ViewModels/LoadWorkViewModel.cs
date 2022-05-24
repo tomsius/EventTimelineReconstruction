4047d91b3aa906ddaf07076285f91755774a67d7
Tomas Kašelynas <tomaskas98@gmail.com>
2022-05-24 14:13:41 +0300

    Create view and viewmodel to laod work

---
+++ EventTimelineReconstruction/EventTimelineReconstruction/ViewModels/LoadWorkViewModel.cs
@@ -0,0 +1,31 @@
+﻿using EventTimelineReconstruction.Commands;
+using EventTimelineReconstruction.Stores;
+using System.Windows.Input;
+
+namespace EventTimelineReconstruction.ViewModels;
+public class LoadWorkViewModel : ViewModelBase, IFileSelectable
+{
+    private string _fileName;
+
+    public string FileName
+    {
+        get
+        {
+            return _fileName;
+        }
+        set
+        {
+            _fileName = value;
+            this.OnPropertyChanged(nameof(FileName));
+        }
+    }
+
+    public ICommand ChooseFileCommand { get; }
+    public ICommand LoadCommand { get; }
+
+    public LoadWorkViewModel(EventTreeViewModel viewModel, EventsStore store)
+    {
+        ChooseFileCommand = new ChooseLoadFileCommand(this);
+        LoadCommand = new LoadWorkCommand(this, store, viewModel);
+    }
+}
