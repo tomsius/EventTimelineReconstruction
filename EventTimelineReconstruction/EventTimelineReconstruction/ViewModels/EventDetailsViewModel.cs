d49ebc40c964dc0fee111f005122661f857ba285
Tomas Kašelynas <tomaskas98@gmail.com>
2022-08-11 14:46:26 +0300

    Refactoring

--- EventTimelineReconstruction/EventTimelineReconstruction/ViewModels/EventDetailsViewModel.cs
+++ EventTimelineReconstruction/EventTimelineReconstruction/ViewModels/EventDetailsViewModel.cs
@@ -16,55 +16,7 @@ public class EventDetailsViewModel : ViewModelBase
         set
         {
             _selectedEvent = value;
-        }
-    }
-
-    public string Date
-    {
-        get
-        {
-            if (_selectedEvent == null) {
-                return string.Empty;
-            }
-
-            DateOnly date = new(_selectedEvent.FullDate.Year, _selectedEvent.FullDate.Month, _selectedEvent.FullDate.Day);
-            return date.ToString();
-        }
-    }
-
-    public string Time
-    {
-        get
-        {
-            if (_selectedEvent == null) {
-                return string.Empty;
-            }
-
-            TimeOnly time = new(_selectedEvent.FullDate.Hour, _selectedEvent.FullDate.Minute, _selectedEvent.FullDate.Second);
-            return time.ToString();
-        }
-    }
-
-    public string Extra
-    {
-        get
-        {
-            if (_selectedEvent == null) {
-                return string.Empty;
-            }
-
-            StringBuilder sb = new();
-
-            foreach (KeyValuePair<string, string> kvp in _selectedEvent.Extra) {
-                sb.Append(kvp.Key);
-                sb.Append('=');
-                sb.Append(kvp.Value);
-                sb.Append('\n');
-            }
-
-            sb.Remove(sb.Length - 1, 1);
-
-            return sb.ToString();
+            this.OnPropertyChanged(nameof(SelectedEvent));
         }
     }
 }
