a7fefc7f6e9175e141289479e37ec096d466e0bb
Tomas Kašelynas <tomaskas98@gmail.com>
2022-08-16 14:20:48 +0300

    Change background colour of tooltip

--- EventTimelineReconstruction/EventTimelineReconstruction/Views/EventTreeView.xaml.cs
+++ EventTimelineReconstruction/EventTimelineReconstruction/Views/EventTreeView.xaml.cs
@@ -40,8 +40,7 @@ namespace EventTimelineReconstruction.Views
                 EventViewModel eventModel = (adornedElement as TreeViewItem).Header as EventViewModel;
                 txt = new(eventModel.DisplayName, CultureInfo.CurrentCulture, FlowDirection.LeftToRight, new Typeface("Segoe UI"), 12, eventModel.Colour, VisualTreeHelper.GetDpi(this).PixelsPerDip);
                 renderRect = new Rect(new Size(txt.Width, txt.Height));
-                rectBrush = new SolidColorBrush(Colors.Silver);
-                rectBrush.Opacity = 0.8;
+                rectBrush = new SolidColorBrush(Colors.White);
             }
             protected override void OnRender(DrawingContext drawingContext)
             {
