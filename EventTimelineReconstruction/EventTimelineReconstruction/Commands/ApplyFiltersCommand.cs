2fe517aa30e55d51580907701f70c13e69b474d3
Tomas Kašelynas <tomaskas98@gmail.com>
2022-08-25 12:49:16 +0300

    Wire up command for FilterView

---
+++ EventTimelineReconstruction/EventTimelineReconstruction/Commands/ApplyFiltersCommand.cs
@@ -0,0 +1,25 @@
+﻿using EventTimelineReconstruction.Stores;
+using EventTimelineReconstruction.ViewModels;
+
+namespace EventTimelineReconstruction.Commands;
+
+public class ApplyFiltersCommand : CommandBase
+{
+    private FilterViewModel _filterViewModel;
+    private readonly FilteringStore _filteringStore;
+
+    public ApplyFiltersCommand(FilterViewModel filterViewModel, FilteringStore filteringStore)
+    {
+        _filterViewModel = filterViewModel;
+        _filteringStore = filteringStore;
+    }
+
+    public override void Execute(object parameter)
+    {
+        _filteringStore.AreAllFiltersApplied = _filterViewModel.AreAllFiltersApplied;
+        _filteringStore.Keyword = _filterViewModel.Keyword;
+        _filteringStore.FromDate = _filterViewModel.FullFromDate;
+        _filteringStore.ToDate = _filterViewModel.FullToDate;
+        _filteringStore.SetEventTypes(_filterViewModel.ChosenEventTypes);
+    }
+}
