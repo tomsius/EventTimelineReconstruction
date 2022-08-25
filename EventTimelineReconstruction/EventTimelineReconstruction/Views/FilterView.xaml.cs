e4ca1fde22b167747860c593f5b3c8b01e7ee880
Tomas Kašelynas <tomaskas98@gmail.com>
2022-08-25 12:47:15 +0300

    Create filtering window

---
+++ EventTimelineReconstruction/EventTimelineReconstruction/Views/FilterView.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel;
+using System.Windows;
+
+namespace EventTimelineReconstruction.Views;
+
+public partial class FilterView : Window
+{
+    public FilterView()
+    {
+        this.InitializeComponent();
+    }
+
+    private void Window_Closing(object sender, CancelEventArgs e)
+    {
+        e.Cancel = true;
+        Visibility = Visibility.Hidden;
+    }
+
+    private void CloseButton_Click(object sender, RoutedEventArgs e)
+    {
+        Visibility = Visibility.Hidden;
+    }
+}
