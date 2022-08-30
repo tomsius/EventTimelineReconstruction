a81900c20ea0e170af78a915ba2c03145399d161
Tomas Kašelynas <tomaskas98@gmail.com>
2022-08-30 12:48:18 +0300

    Create ColouringStore to save all colours by event type

---
+++ EventTimelineReconstruction/EventTimelineReconstruction/Stores/ColouringStore.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Windows.Media;
+
+namespace EventTimelineReconstruction.Stores;
+
+public class ColouringStore
+{
+    public Dictionary<string, Brush> ColoursByType { get; }
+
+    public ColouringStore()
+    {
+        ColoursByType = new();
+    }
+
+    public void SetColoursByType(Dictionary<string, Brush> coloursByType)
+    {
+        foreach (KeyValuePair<string, Brush> pair in coloursByType)
+        {
+            string key = pair.Key;
+            Brush value = pair.Value;
+
+            if (ColoursByType.ContainsKey(key))
+            {
+                ColoursByType[key] = value;
+            }
+            else
+            {
+                ColoursByType.Add(key, value);
+            }
+        }
+    }
+}
