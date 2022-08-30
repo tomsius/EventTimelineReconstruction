d079561ce28e8866de051d1c7384c789016470b2
Tomas Kašelynas <tomaskas98@gmail.com>
2022-08-30 12:49:04 +0300

    Save selected colours by type in store

---
+++ EventTimelineReconstruction/EventTimelineReconstruction/Commands/ApplyColourOptionsCommand.cs
@@ -0,0 +1,21 @@
+﻿using EventTimelineReconstruction.Stores;
+using EventTimelineReconstruction.ViewModels;
+
+namespace EventTimelineReconstruction.Commands;
+
+public class ApplyColourOptionsCommand : CommandBase
+{
+    private readonly ColourViewModel _colourViewModel;
+    private readonly ColouringStore _colouringStore;
+
+    public ApplyColourOptionsCommand(ColourViewModel colourViewModel, ColouringStore colouringStore)
+    {
+        _colourViewModel = colourViewModel;
+        _colouringStore = colouringStore;
+    }
+
+    public override void Execute(object parameter)
+    {
+        _colouringStore.SetColoursByType(_colourViewModel.ColoursByType);
+    }
+}
