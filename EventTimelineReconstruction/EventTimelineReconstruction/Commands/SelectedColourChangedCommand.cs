9b5ff2423c9bd8d0ab5026eaf97b5eb635f3007f
Tomas Kašelynas <tomaskas98@gmail.com>
2022-08-30 12:44:36 +0300

    Create event triggers to save selected colours

---
+++ EventTimelineReconstruction/EventTimelineReconstruction/Commands/SelectedColourChangedCommand.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using EventTimelineReconstruction.Utils;
+using EventTimelineReconstruction.ViewModels;
+using Xceed.Wpf.Toolkit;
+
+namespace EventTimelineReconstruction.Commands;
+
+public class SelectedColourChangedCommand : CommandBase
+{
+    private readonly ColourViewModel _colourViewModel;
+
+    public SelectedColourChangedCommand(ColourViewModel colourViewModel)
+    {
+        _colourViewModel = colourViewModel;
+    }
+
+    public override void Execute(object parameter)
+    {
+        RoutedEventArgs e = parameter as RoutedEventArgs;
+        ColorPicker colorPicker = e.OriginalSource as ColorPicker;
+        TextBlock textBlock = ColouringUtils.GetSiblingTextBlock(colorPicker);
+
+        string key = textBlock.Text;
+        Brush value = new SolidColorBrush(colorPicker.SelectedColor ?? Colors.Black);
+        _colourViewModel.UpdateColourByType(key, value);
+    }
+}
