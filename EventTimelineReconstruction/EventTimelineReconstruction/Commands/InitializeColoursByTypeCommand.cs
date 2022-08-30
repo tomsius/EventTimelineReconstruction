e41c0038b203c2382e46ce9dfbcc36e96f3a975e
Tomas Kašelynas <tomaskas98@gmail.com>
2022-08-30 12:48:47 +0300

    Initialize command to fill default colours by type

---
+++ EventTimelineReconstruction/EventTimelineReconstruction/Commands/InitializeColoursByTypeCommand.cs
@@ -0,0 +1,33 @@
+﻿using EventTimelineReconstruction.Utils;
+using System.Collections.Generic;
+using System.Windows.Controls;
+using System.Windows;
+using EventTimelineReconstruction.ViewModels;
+using Xceed.Wpf.Toolkit;
+using System.Windows.Media;
+
+namespace EventTimelineReconstruction.Commands;
+
+public class InitializeColoursByTypeCommand : CommandBase
+{
+    private readonly ColourViewModel _colourViewModel;
+
+    public InitializeColoursByTypeCommand(ColourViewModel colourViewModel)
+    {
+        _colourViewModel = colourViewModel;
+    }
+
+    public override void Execute(object parameter)
+    {
+        RoutedEventArgs e = parameter as RoutedEventArgs;
+        Dictionary<TextBlock, ColorPicker> children = ColouringUtils.GetChildrenElements(e.OriginalSource as StackPanel);
+
+        foreach (KeyValuePair<TextBlock, ColorPicker> pair in children)
+        {
+            string key = pair.Key.Text;
+            Brush value = new SolidColorBrush(pair.Value.SelectedColor ?? Colors.Black);
+
+            _colourViewModel.UpdateColourByType(key, value);
+        }
+    }
+}
