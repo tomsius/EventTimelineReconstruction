e183ed62fa980ad090a3c7e075b2a3f22e5483bb
Tomas Kašelynas <tomaskas98@gmail.com>
2022-09-02 11:38:28 +0300

    Calculate SHA256 hash function of file

---
+++ EventTimelineReconstruction/EventTimelineReconstruction/Services/SHA256Calculator.cs
@@ -0,0 +1,17 @@
+﻿using System.IO;
+using System.Security.Cryptography;
+
+namespace EventTimelineReconstruction.Services;
+
+public class SHA256Calculator : IHashCalculator
+{
+    public byte[] Calculate(string filePath)
+    {
+        using SHA256 hashFunction = SHA256.Create();
+        using FileStream fileStream = File.OpenRead(filePath);
+        fileStream.Position = 0;
+
+        byte[] hashValue = hashFunction.ComputeHash(fileStream);
+        return hashValue;
+    }
+}
