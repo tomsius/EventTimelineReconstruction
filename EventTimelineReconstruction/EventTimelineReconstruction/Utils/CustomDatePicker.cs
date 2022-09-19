d5a5fdac14d6df5acf6daa370d6a83ccb048c9a4
Tomas Kašelynas <tomaskas98@gmail.com>
2022-09-19 15:27:37 +0300

    Change watermark text for DatePicker control

---
+++ EventTimelineReconstruction/EventTimelineReconstruction/Utils/CustomDatePicker.cs
@@ -0,0 +1,32 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+
+namespace EventTimelineReconstruction.Utils;
+
+public class CustomDatePicker : DatePicker
+{
+    public string PlaceholderText
+    {
+        get
+        {
+            return (string)GetValue(WatermarkTextProperty);
+        }
+        set
+        {
+            SetValue(WatermarkTextProperty, value);
+        }
+    }
+
+    public readonly static DependencyProperty WatermarkTextProperty = DependencyProperty.Register("PlaceholderText", typeof(string), typeof(CustomDatePicker), new PropertyMetadata("Select a Date"));
+
+    public override void OnApplyTemplate()
+    {
+        base.OnApplyTemplate();
+        DatePickerTextBox box = base.GetTemplateChild("PART_TextBox") as DatePickerTextBox;
+        box.ApplyTemplate();
+
+        ContentControl watermark = box.Template.FindName("PART_Watermark", box) as ContentControl;
+        watermark.Content = PlaceholderText;
+    }
+}
