7ecb9c464cebed7dd8cf31fe5751be4f09d1c257
Tomas Kašelynas <tomaskas98@gmail.com>
2022-09-21 12:28:11 +0300

    Create validator for dates and time

---
+++ EventTimelineReconstruction/EventTimelineReconstruction/Validators/DateValidator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace EventTimelineReconstruction.Validators;
+
+public class TimeValidator : ITimeValidator
+{
+    public bool AreDatesValid(DateTime start, DateTime end)
+    {
+        return start <= end;
+    }
+
+    public bool AreHoursValid(int hours)
+    {
+        return hours >= TimeOnly.MinValue.Hour && hours <= TimeOnly.MaxValue.Hour;
+    }
+
+    public bool AreMinutesValid(int minutes)
+    {
+        return minutes >= TimeOnly.MinValue.Minute && minutes <= TimeOnly.MaxValue.Minute;
+    }
+}
