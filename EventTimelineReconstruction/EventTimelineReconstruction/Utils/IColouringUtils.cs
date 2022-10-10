0619aaf8c23060b5e8e1a9b6595ab07f990a5fe3
Tomas Kašelynas <tomaskas98@gmail.com>
2022-10-10 11:16:07 +0300

    Convert from static ColouringUtils

---
+++ EventTimelineReconstruction/EventTimelineReconstruction/Utils/IColouringUtils.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using System.Windows.Controls;
+using Xceed.Wpf.Toolkit;
+
+namespace EventTimelineReconstruction.Utils;
+public interface IColouringUtils
+{
+    public Dictionary<TextBlock, ColorPicker> GetChildrenElements(StackPanel parent);
+    public TextBlock GetSiblingTextBlock(ColorPicker colorPicker);
+}
