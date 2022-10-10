35683911a111d0e21d598a5fb7a52d9571db3adc
Tomas Kašelynas <tomaskas98@gmail.com>
2022-10-10 16:26:22 +0300

    Create unit tests for TimeValidator

---
+++ EventTimelineReconstruction/EventTimelineReconstruction.UnitTests/Validators/TimeValidatorTests.cs
@@ -0,0 +1,103 @@
+﻿using EventTimelineReconstruction.Validators;
+
+namespace EventTimelineReconstruction.UnitTests.Validators;
+
+[TestClass]
+public class TimeValidatorTests
+{
+    [TestMethod]
+    public void AreDatesValid_ShouldReturnTrue_WhenDateAreInOrder()
+    {
+        // Arrange
+        TimeValidator timeValidator = new();
+        DateTime from = new DateTime(1998, 10, 14, 11, 45, 25);
+        DateTime to = new DateTime(2022, 5, 9, 23, 11, 58);
+
+        // Act
+        bool result = timeValidator.AreDatesValid(from, to);
+
+        // Assert
+        Assert.IsTrue(result);
+    }
+
+    [TestMethod]
+    public void AreDatesValid_ShouldReturnFalse_WhenDateAreOutOfOrder()
+    {
+        // Arrange
+        TimeValidator timeValidator = new();
+        DateTime from = new DateTime(2022, 5, 9, 23, 11, 58);
+        DateTime to = new DateTime(1998, 10, 14, 11, 45, 25);
+
+        // Act
+        bool result = timeValidator.AreDatesValid(from, to);
+
+        // Assert
+        Assert.IsFalse(result);
+    }
+
+    [DataTestMethod]
+    [DataRow(0)]
+    [DataRow(12)]
+    [DataRow(23)]
+    public void AreHoursValid_ShouldReturnTrue_WhenHoursAreBetween0And23(int hours)
+    {
+        // Arrange
+        TimeValidator timeValidator = new();
+
+        // Act
+        bool result = timeValidator.AreHoursValid(hours);
+
+        // Assert
+        Assert.IsTrue(result);
+    }
+
+    [DataTestMethod]
+    [DataRow(-10)]
+    [DataRow(-1)]
+    [DataRow(24)]
+    [DataRow(40)]
+    public void AreHoursValid_ShouldReturnFalse_WhenHoursAreNotBetween0And23(int hours)
+    {
+        // Arrange
+        TimeValidator timeValidator = new();
+
+        // Act
+        bool result = timeValidator.AreHoursValid(hours);
+
+        // Assert
+        Assert.IsFalse(result);
+    }
+
+    [DataTestMethod]
+    [DataRow(0)]
+    [DataRow(33)]
+    [DataRow(59)]
+    public void AreMinutesValid_ShouldReturnTrue_WhenMinutesAreBetween0And59(int minutes)
+    {
+        // Arrange
+        TimeValidator timeValidator = new();
+
+        // Act
+        bool result = timeValidator.AreMinutesValid(minutes);
+
+        // Assert
+        Assert.IsTrue(result);
+    }
+
+    [DataTestMethod]
+    [DataRow(-10)]
+    [DataRow(-1)]
+    [DataRow(60)]
+    [DataRow(125)]
+    public void AreMinutesValid_ShouldReturnFalse_WhenMinutesAreNotBetween0And59(int minutes)
+    {
+        // Arrange
+        TimeValidator timeValidator = new();
+
+        // Act
+        bool result = timeValidator.AreMinutesValid(minutes);
+
+        // Assert
+        Assert.IsFalse(result);
+    }
+}
