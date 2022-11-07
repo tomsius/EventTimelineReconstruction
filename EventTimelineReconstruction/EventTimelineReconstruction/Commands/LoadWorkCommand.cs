69f3784d947e6b434fabf47b8b9a4631d9027f56
Tomas Kašelynas <tomaskas98@gmail.com>
2022-11-07 12:55:19 +0200

    Check if given file exists and show error message if work cannot be imported

--- EventTimelineReconstruction/EventTimelineReconstruction/Commands/LoadWorkCommand.cs
+++ EventTimelineReconstruction/EventTimelineReconstruction/Commands/LoadWorkCommand.cs
@@ -1,9 +1,12 @@
 ﻿using EventTimelineReconstruction.Services;
 using EventTimelineReconstruction.Stores;
 using EventTimelineReconstruction.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.IO;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace EventTimelineReconstruction.Commands;
 
@@ -30,13 +33,34 @@ public class LoadWorkCommand : AsyncCommandBase
 
     public override async Task ExecuteAsync(object parameter)
     {
+        if (!File.Exists(_loadWorkViewModel.FileName))
+        {
+            string message = $"{(string)App.Current.Resources["FileNotFound_Message"]} {_loadWorkViewModel.FileName}";
+            string caption = (string)App.Current.Resources["FileNotFound_Caption"];
+
+            MessageBox.Show(message, caption, MessageBoxButton.OK);
+            return;
+        }
+
         _loadWorkViewModel.IsLoading = true;
 
-        List<EventViewModel> loadedEvents = await _workLoader.LoadWork(_loadWorkViewModel.FileName);
-        _store.LoadEvents(loadedEvents);
-        _eventTreeViewModel.LoadEvents(loadedEvents);
+        try
+        {
+            List<EventViewModel> loadedEvents = await _workLoader.LoadWork(_loadWorkViewModel.FileName);
+            _store.LoadEvents(loadedEvents);
+            _eventTreeViewModel.LoadEvents(loadedEvents);
+        }
+        catch (Exception)
+        {
+            string message = (string)App.Current.Resources["LoadingError_Message"];
+            string caption = (string)App.Current.Resources["LoadingError_Caption"];
 
-        _loadWorkViewModel.IsLoading = false;
+            MessageBox.Show(message, caption, MessageBoxButton.OK);
+        }
+        finally
+        {
+            _loadWorkViewModel.IsLoading = false;
+        }
     }
 
     private void OnViewModelPropertyChanged(object sender, PropertyChangedEventArgs e)
