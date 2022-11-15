2ea85b747a147a8434bb52ae1c9c820c35e985b9
Tomas Kašelynas <tomaskas98@gmail.com>
2022-11-15 13:41:21 +0200

    Create HighLevelEventViewModel

---
+++ EventTimelineReconstruction/EventTimelineReconstruction/ViewModels/HighLevelEventViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace EventTimelineReconstruction.ViewModels;
+
+public class HighLevelEventViewModel
+{
+    public DateOnly Date { get; set; }
+    public TimeOnly Time { get; set; }
+    public string Source { get; set; }
+    public string Short { get; set; }
+    public string Visit { get; set; }
+    public int Reference { get; set; }
+
+    public HighLevelEventViewModel()
+    {
+        Visit = "-";
+    }
+}
