365be87983783ee98af5c963b169dd7d29e2549e
Tomas Kašelynas <tomaskas98@gmail.com>
2022-11-27 13:39:02 +0200

    Create viewmodel for low level event

---
+++ EventTimelineReconstruction/EventTimelineReconstruction/ViewModels/LowLevelEventViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace EventTimelineReconstruction.ViewModels;
+
+public class LowLevelEventViewModel
+{
+    public DateOnly Date { get; set; }
+    public TimeOnly Time { get; set; }
+    public string Source { get; set; }
+    public string Short { get; set; }
+    public string Visit { get; set; }
+    public string Extra { get; set; }
+    public string Reference { get; set; }
+
+    public LowLevelEventViewModel()
+    {
+        Visit = "-";
+        Extra = "-";
+    }
+}
