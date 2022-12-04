3924934d5187c187a9184da938a6581a828f3b91
Tomas Kašelynas <tomaskas98@gmail.com>
2022-12-04 13:04:38 +0200

    Create HighLevelArtefactsAbstractorUtils

---
+++ EventTimelineReconstruction/EventTimelineReconstruction/Utils/IHighLevelArtefactsAbstractorUtils.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using EventTimelineReconstruction.ViewModels;
+
+namespace EventTimelineReconstruction.Utils;
+
+public interface IHighLevelArtefactsAbstractorUtils
+{
+    string GetDescriptionFromLogSource(EventViewModel eventViewModel);
+    string GetDescriptionFromMetaSource(string description);
+    string GetDescriptionFromRegSource(string sourceType, string description);
+    int GetFileCountInRowAtSameMinute(List<EventViewModel> events, int startIndex);
+    string GetFilename(string description);
+    string GetOtherBrowserExtraFromWebhistSource(Dictionary<string, string> extra);
+    List<int> GetValidFileEventIndices(List<EventViewModel> events, int startIndex, int endIndex);
+    bool IsFileDuplicateOfLnk(List<EventViewModel> events, int startIndex, EventViewModel current);
+    bool IsPeLineExecutable(EventViewModel eventViewModel);
+    bool IsPeLineValid(EventViewModel eventViewModel);
+}
