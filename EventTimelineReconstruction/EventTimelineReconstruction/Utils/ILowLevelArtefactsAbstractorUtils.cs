7f5e61ad455a2a13e882a85799fec24fcd88aed8
Tomas Kašelynas <tomaskas98@gmail.com>
2022-12-09 13:33:26 +0200

    Create utilities for LowLevelArtefactsAbstractor

---
+++ EventTimelineReconstruction/EventTimelineReconstruction/Utils/ILowLevelArtefactsAbstractorUtils.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace EventTimelineReconstruction.Utils;
+
+public interface ILowLevelArtefactsAbstractorUtils
+{
+    string GetAddress(string description);
+    string GetExtraValue(Dictionary<string, string> extra);
+    bool IsValidWebhistLine(string sourceType, string type);
+}
