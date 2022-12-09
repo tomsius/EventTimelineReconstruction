ebbd465febd8331727b5eadbb8b51d8225d9f3dc
Tomas Kašelynas <tomaskas98@gmail.com>
2022-12-09 13:32:55 +0200

    Create ViewModel for low level artefact

---
+++ EventTimelineReconstruction/EventTimelineReconstruction/ViewModels/LowLevelArtefactViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace EventTimelineReconstruction.ViewModels;
+
+public class LowLevelArtefactViewModel
+{
+    public DateOnly Date { get; set; }
+    public TimeOnly Time { get; set; }
+    public string Timezone { get; set; }
+    public string Macb { get; set; }
+    public string Source { get; set; }
+    public string SourceType { get; set; }
+    public string Type { get; set; }
+    public string User { get; set; }
+    public string Host { get; set; }
+    public string Short { get; set; }
+    public string Description { get; set; }
+    public string Version { get; set; }
+    public string Filename { get; set; }
+    public string Inode { get; set; }
+    public string Notes { get; set; }
+    public string Format { get; set; }
+    public string Extra { get; set; }
+    public string Reference { get; set; }
+}
