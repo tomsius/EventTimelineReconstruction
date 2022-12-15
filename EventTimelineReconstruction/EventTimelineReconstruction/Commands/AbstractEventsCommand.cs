62b3f078e3e201841edd763668b0c62121488acf
Tomas Kašelynas <tomaskas98@gmail.com>
2022-12-15 14:07:24 +0200

    Wire up commands

---
+++ EventTimelineReconstruction/EventTimelineReconstruction/Commands/AbstractEventsCommand.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Threading;
+using EventTimelineReconstruction.Abstractors;
+using EventTimelineReconstruction.Stores;
+using EventTimelineReconstruction.ViewModels;
+
+namespace EventTimelineReconstruction.Commands;
+
+public class AbstractEventsCommand : AsyncCommandBase
+{
+    private readonly AbstractedEventsViewModel _viewModel;
+    private readonly IEventsStore _store;
+    private readonly IHighLevelEventsAbstractor _highLevelEventsAbstractor;
+    private readonly ILowLevelEventsAbstractor _lowLevelEventsAbstractor;
+    private readonly IHighLevelArtefactsAbstractor _highLevelArtefactsAbstractor;
+    private readonly ILowLevelArtefactsAbstractor _lowLevelArtefactsAbstractor;
+
+    public AbstractEventsCommand(AbstractedEventsViewModel viewModel, IEventsStore store, IHighLevelEventsAbstractor highLevelEventsAbstractor, ILowLevelEventsAbstractor lowLevelEventsAbstractor, IHighLevelArtefactsAbstractor highLevelArtefactsAbstractor, ILowLevelArtefactsAbstractor lowLevelArtefactsAbstractor)
+    {
+        _viewModel = viewModel;
+        _store = store;
+        _highLevelEventsAbstractor = highLevelEventsAbstractor;
+        _lowLevelEventsAbstractor = lowLevelEventsAbstractor;
+        _highLevelArtefactsAbstractor = highLevelArtefactsAbstractor;
+        _lowLevelArtefactsAbstractor = lowLevelArtefactsAbstractor;
+    }
+
+    public override async Task ExecuteAsync(object parameter)
+    {
+        _viewModel.IsAbstracting = true;
+
+        List<EventViewModel> events = _store.GetStoredEventViewModelsAsOneLevel();
+
+        await Application.Current.Dispatcher.BeginInvoke(DispatcherPriority.Background, () =>
+        {
+            List<HighLevelEventViewModel> highLevelEvents = _highLevelEventsAbstractor.FormHighLevelEvents(events);
+            _viewModel.LoadHighLevelEvents(highLevelEvents);
+        });
+
+        await Application.Current.Dispatcher.BeginInvoke(DispatcherPriority.Background, () =>
+        {
+            List<LowLevelEventViewModel> lowLevelEvents = _lowLevelEventsAbstractor.FormLowLevelEvents(events);
+            _viewModel.LoadLowLevelEvents(lowLevelEvents);
+        });
+
+        await Application.Current.Dispatcher.BeginInvoke(DispatcherPriority.Background, () =>
+        {
+            List<HighLevelArtefactViewModel> highLevelArtefacts = _highLevelArtefactsAbstractor.FormHighLevelArtefacts(events);
+            _viewModel.LoadHighLevelArtefacts(highLevelArtefacts);
+        });
+
+        await Application.Current.Dispatcher.BeginInvoke(DispatcherPriority.Background, () =>
+        {
+            List<LowLevelArtefactViewModel> lowLevelArtefacts = _lowLevelArtefactsAbstractor.FormLowLevelArtefacts(events);
+            _viewModel.LoadLowLevelArtefacts(lowLevelArtefacts);
+        });
+
+        _viewModel.IsAbstracting = false;
+    }
+}
---
+++ EventTimelineReconstruction/EventTimelineReconstruction/Commands/AbstractLowLevelArtefactsCommand.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using EventTimelineReconstruction.Abstractors;
+using EventTimelineReconstruction.Stores;
+using EventTimelineReconstruction.ViewModels;
+
+namespace EventTimelineReconstruction.Commands;
+
+public class AbstractLowLevelArtefactsCommand : CommandBase
+{
+    private readonly AbstractedEventsViewModel _viewModel;
+    private readonly IEventsStore _store;
+    private readonly ILowLevelArtefactsAbstractor _abstractor;
+
+    public AbstractLowLevelArtefactsCommand(AbstractedEventsViewModel abstractedEventsViewModel, IEventsStore store, ILowLevelArtefactsAbstractor lowLevelArtefactsAbstractor)
+    {
+        _viewModel = abstractedEventsViewModel;
+        _store = store;
+        _abstractor = lowLevelArtefactsAbstractor;
+
+        _viewModel.PropertyChanged += this.OnViewModelPropertyChanged;
+    }
+
+    public override bool CanExecute(object parameter)
+    {
+        return !_viewModel.HasErrors;
+    }
+
+    public override void Execute(object parameter)
+    {
+        _viewModel.IsAbstracting = true;
+
+        List<EventViewModel> events = _store.GetStoredEventViewModelsAsOneLevel();
+        List<LowLevelArtefactViewModel> lowLevelArtefacts = _abstractor.FormLowLevelArtefacts(events, double.Parse(_viewModel.Threshold));
+        _viewModel.LoadLowLevelArtefacts(lowLevelArtefacts);
+
+        _viewModel.IsAbstracting = false;
+    }
+
+    private void OnViewModelPropertyChanged(object sender, PropertyChangedEventArgs e)
+    {
+        if (e.PropertyName == nameof(AbstractedEventsViewModel.Threshold))
+        {
+            this.OnCanExecuteChanged();
+        }
+    }
+}
