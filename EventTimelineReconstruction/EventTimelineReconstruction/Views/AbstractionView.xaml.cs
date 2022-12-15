b863ffa32626ee9a673fd3606720e0f8315e75dc
Tomas Kašelynas <tomaskas98@gmail.com>
2022-12-15 14:07:16 +0200

    Create view for abstraction levels

---
+++ EventTimelineReconstruction/EventTimelineReconstruction/Views/AbstractionView.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel;
+using System.Windows;
+
+namespace EventTimelineReconstruction.Views;
+/// <summary>
+/// Interaction logic for AbstractionView.xaml
+/// </summary>
+public partial class AbstractionView : Window
+{
+    public AbstractionView()
+    {
+        InitializeComponent();
+    }
+
+    private void Window_Closing(object sender, CancelEventArgs e)
+    {
+        e.Cancel = true;
+        Visibility = Visibility.Hidden;
+    }
+}
