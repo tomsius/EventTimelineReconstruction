10b945f416e9d8ba31608c92ffbfd9e1bd62e893
Tomas Kašelynas <tomaskas98@gmail.com>
2023-04-12 14:48:04 +0300

    Update work saver test

--- EventTimelineReconstruction/EventTimelineReconstruction.Tests/IntegrationTests/Services/FileWorkSaverTests.cs
+++ EventTimelineReconstruction/EventTimelineReconstruction.Tests/IntegrationTests/Services/FileWorkSaverTests.cs
@@ -64,6 +64,48 @@ public class FileWorkSaverTests
                     "Format2",
                     new Dictionary<string, string>() { { "Key21", "Value21" }, { "Key22", "Value22" } },
                     2
+                )),
+            new EventViewModel(
+                new EventModel(
+                    new DateOnly(2022, 10, 14),
+                    new TimeOnly(10, 52),
+                    TimeZoneInfo.Utc,
+                    "MACB4",
+                    "Source4",
+                    "Source Type4",
+                    "Type4",
+                    "Username4",
+                    "Hostname4",
+                    "Short Description4",
+                    "Full Description4",
+                    2.5,
+                    "Filename4",
+                    "iNode number4",
+                    "Notes4",
+                    "Format4",
+                    new Dictionary<string, string>() { { "Key41", "Value41" }, { "Key42", "Value42" } },
+                    4
+                )),
+            new EventViewModel(
+                new EventModel(
+                    new DateOnly(2022, 10, 14),
+                    new TimeOnly(10, 52),
+                    TimeZoneInfo.Utc,
+                    "MACB5",
+                    "Source5",
+                    "Source Type5",
+                    "Type5",
+                    "Username5",
+                    "Hostname5",
+                    "Short Description5",
+                    "Full Description5",
+                    2.5,
+                    "Filename5",
+                    "iNode number5",
+                    "Notes5",
+                    "Format5",
+                    new Dictionary<string, string>() { { "Key51", "Value51" }, { "Key52", "Value52" } },
+                    5
                 ))
         };
         EventViewModel child = new(
@@ -112,6 +154,8 @@ public class FileWorkSaverTests
             "2020,4,6,5,23,0,UTC;0;(UTC) Coordinated Universal Time;Coordinated Universal Time;Coordinated Universal Time;;,MACB1,Source1,Source Type1,Type1,Username1,Hostname1,Short Description1,Full Description1,2.5,Filename1,iNode number1,Notes1,Format1,Key11:Value11;Key12:Value12,1,True,#FF000000",
             "\t2021,12,1,17,53,0,UTC;0;(UTC) Coordinated Universal Time;Coordinated Universal Time;Coordinated Universal Time;;,MACB3,Source3,Source Type3,Type3,Username3,Hostname3,Short Description3,Full Description3,2.5,Filename3,iNode number3,Notes3,Format3,Key31:Value31;Key32:Value32,3,True,#FF000000",
             "2022,10,14,10,52,0,UTC;0;(UTC) Coordinated Universal Time;Coordinated Universal Time;Coordinated Universal Time;;,MACB2,Source2,Source Type2,Type2,Username2,Hostname2,Short Description2,Full Description2,2.5,Filename2,iNode number2,Notes2,Format2,Key21:Value21;Key22:Value22,2,True,#FF000000",
+            "2022,10,14,10,52,0,UTC;0;(UTC) Coordinated Universal Time;Coordinated Universal Time;Coordinated Universal Time;;,MACB4,Source4,Source Type4,Type4,Username4,Hostname4,Short Description4,Full Description4,2.5,Filename4,iNode number4,Notes4,Format4,Key41:Value41;Key42:Value42,4,True,#FF000000",
+            "2022,10,14,10,52,0,UTC;0;(UTC) Coordinated Universal Time;Coordinated Universal Time;Coordinated Universal Time;;,MACB5,Source5,Source Type5,Type5,Username5,Hostname5,Short Description5,Full Description5,2.5,Filename5,iNode number5,Notes5,Format5,Key51:Value51;Key52:Value52,5,True,#FF000000",
             "",
             "2020,1,1,4,20,54,Source4,Short4,Visit4,4",
             "2020,1,2,4,21,4,Source5,Short5,Visit5,5",
