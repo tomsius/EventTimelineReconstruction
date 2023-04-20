b10008a0565aa908d882f3e96cb2ce633d248359
Tomas Kašelynas <tomaskas98@gmail.com>
2023-04-20 12:15:25 +0300

    Benchmark low levle events with chain of responsibility

--- EventTimelineReconstruction/EventTimelineReconstruction.Benchmarks/LowLevelEventsBenchmarks.cs
+++ EventTimelineReconstruction/EventTimelineReconstruction.Benchmarks/LowLevelEventsBenchmarks.cs
@@ -1,5 +1,7 @@
 ﻿using BenchmarkDotNet.Attributes;
 using BenchmarkDotNet.Order;
+using EventTimelineReconstruction.Benchmarks.ChainOfResponsibility;
+using EventTimelineReconstruction.Benchmarks.ChainOfResponsibility.LowLevelEvents;
 using EventTimelineReconstruction.Benchmarks.Models;
 using EventTimelineReconstruction.Benchmarks.Utils;
 
@@ -16,12 +18,33 @@ public class LowLevelEventsBenchmarks
     private List<EventViewModel> _events;
     private IHighLevelEventsAbstractorUtils _highLevelAbstractorUtils;
     private ILowLevelEventsAbstractorUtils _lowLevelAbstractorUtils;
+    private IHandler _handler;
 
     [GlobalSetup]
     public void GlobalSetup()
     {
         _highLevelAbstractorUtils = new HighLevelEventsAbstractorUtils();
         _lowLevelAbstractorUtils = new LowLevelEventsAbstractorUtils();
+
+        ILowWebhistEventHandler webhistHandler = new LowWebhistEventHandler(_highLevelAbstractorUtils, _lowLevelAbstractorUtils);
+        ILowFileEventHandler fileHandler = new LowFileEventHandler(_lowLevelAbstractorUtils);
+        ILowLnkEventHandler lnkHandler = new LowLnkEventHandler(_highLevelAbstractorUtils, _lowLevelAbstractorUtils);
+        ILowLogEventHandler logHandler = new LowLogEventHandler(_lowLevelAbstractorUtils);
+        ILowMetaEventHandler metaHandler = new LowMetaEventHandler(_lowLevelAbstractorUtils);
+        ILowRegEventHandler regHandler = new LowRegEventHandler(_lowLevelAbstractorUtils);
+        ILowOlecfEventHandler olecfHandler = new LowOlecfEventHandler();
+        ILowPeEventHandler peHandler = new LowPeEventHandler(_highLevelAbstractorUtils);
+        ILowRecbinEventHandler recbinHandler = new LowRecbinEventHandler(_lowLevelAbstractorUtils);
+
+        _handler = webhistHandler;
+        webhistHandler.Next = fileHandler;
+        fileHandler.Next = lnkHandler;
+        lnkHandler.Next = logHandler;
+        logHandler.Next = metaHandler;
+        metaHandler.Next = regHandler;
+        regHandler.Next = olecfHandler;
+        olecfHandler.Next = peHandler;
+        peHandler.Next = recbinHandler;
 
         // sukurti ivykiu sarasa
         _events = new(N);
@@ -567,5 +590,224 @@ public class LowLevelEventsBenchmarks
         };
 
         return result;
+    }
+
+    [Benchmark]
+    public List<ISerializableLevel> FormLowLevelEvents_ChainOfResponsibility()
+    {
+        List<ISerializableLevel> lowLevelEvents = new(_events.Count);
+
+        for (int i = 0; i < _events.Count; i++)
+        {
+            if (_events[i].Source == "OLECF")
+            {
+                while (i < _events.Count - 1 && AreOlecfEventsOfSameTime_ChainOfResponsibility(_events[i], _events[i + 1]))
+                {
+                    i++;
+                }
+            }
+
+            ISerializableLevel lowLevelEvent = _handler.FormAbstractEvent(_events, lowLevelEvents, _events[i]);
+
+            if (_events[i].Source == "FILE")
+            {
+                int validEventIndex = this.GetValidFileEventIndex_ChainOfResponsibility(_events, i);
+
+                if (validEventIndex != -1 && !_lowLevelAbstractorUtils.DoesNeedComposing(_events, i - 1, _events[validEventIndex]))
+                {
+                    int offset = validEventIndex - i;
+                    i += offset;
+
+                    bool isSearchedFormat = _events[i].Format == "lnk/shell_items" || _events[i].Format == "filestat";
+                    if (isSearchedFormat)
+                    {
+                        DateTime date = _events[i].FullDate;
+
+                        while (i < _events.Count - 1 && _events[i].Source == "FILE" && _events[i + 1].FullDate.CompareTo(date) == 0)
+                        {
+                            i++;
+                        }
+                    }
+                }
+            }
+
+            if (lowLevelEvent is not null)
+            {
+                lowLevelEvents.Add(lowLevelEvent);
+                NormalizeEvents_ChainOfResponsibility(lowLevelEvents, _events, _events[i]);
+            }
+        }
+
+        return lowLevelEvents;
+    }
+
+    private static void NormalizeEvents_ChainOfResponsibility(List<ISerializableLevel> lowLevelEvents, List<EventViewModel> events, EventViewModel currentEvent)
+    {
+        if (lowLevelEvents.Count < 2)
+        {
+            return;
+        }
+
+        int eventIndex = GetEventIndex_ChainOfResponsibility(events, ((LowLevelEventViewModel)lowLevelEvents[^2]).Reference);
+        EventViewModel lastEvent = events[eventIndex];
+
+        if (lastEvent.FullDate.CompareTo(currentEvent.FullDate) != 0 || ((LowLevelEventViewModel)lowLevelEvents[^2]).Short != ((LowLevelEventViewModel)lowLevelEvents[^1]).Short)
+        {
+            return;
+        }
+
+        if (lastEvent.MACB.Contains('B') && !currentEvent.MACB.Contains('B'))
+        {
+            lowLevelEvents.RemoveAt(lowLevelEvents.Count - 1);
+            return;
+        }
+
+        if (!lastEvent.MACB.Contains('B') && currentEvent.MACB.Contains('B'))
+        {
+            lowLevelEvents.RemoveAt(lowLevelEvents.Count - 2);
+            return;
+        }
+
+        if (lastEvent.Filename.Contains(".lnk") && !currentEvent.Filename.Contains(".lnk"))
+        {
+            lowLevelEvents.RemoveAt(lowLevelEvents.Count - 1);
+            return;
+        }
+
+        if (!lastEvent.Filename.Contains(".lnk") && currentEvent.Filename.Contains(".lnk"))
+        {
+            lowLevelEvents.RemoveAt(lowLevelEvents.Count - 2);
+            return;
+        }
+
+        if (lastEvent.Source == "REG" && currentEvent.Source == "REG")
+        {
+            lowLevelEvents.RemoveAt(lowLevelEvents.Count - 1);
+            return;
+        }
+
+        if ((lastEvent.Source == "REG" || lastEvent.Source == "OLECF" || lastEvent.Source == "LOG") && currentEvent.Source == "LOG")
+        {
+            lowLevelEvents.RemoveAt(lowLevelEvents.Count - 2);
+            return;
+        }
+
+        if (lastEvent.Source == "LOG" && (currentEvent.Source == "REG" || currentEvent.Source == "OLECF" || currentEvent.Source == "LOG"))
+        {
+            lowLevelEvents.RemoveAt(lowLevelEvents.Count - 1);
+            return;
+        }
+
+        lowLevelEvents.RemoveAt(lowLevelEvents.Count - 1);
+    }
+
+    private static int GetEventIndex_ChainOfResponsibility(List<EventViewModel> events, int reference)
+    {
+        int index = -1;
+
+        for (int i = events.Count - 1; i >= 0; i--)
+        {
+            if (events[i].SourceLine == reference)
+            {
+                index = i;
+                break;
+            }
+        }
+
+        return index;
+    }
+
+    private int GetValidFileEventIndex_ChainOfResponsibility(List<EventViewModel> events, int startIndex)
+    {
+        for (int i = startIndex; i < events.Count; i++)
+        {
+            if (events[i].Source != "FILE")
+            {
+                break;
+            }
+
+            if (_lowLevelAbstractorUtils.IsValidFileEvent(events[startIndex]))
+            {
+                if (events[startIndex].SourceType == "File entry shell item")
+                {
+                    return GetIndexOfLastFileEntryShellItemEventOfSameTime_ChainOfResponsibility(events, startIndex);
+                }
+                else
+                {
+                    return this.GetIndexOfLastFileEventOfSameTime_ChainOfResponsibility(events, startIndex);
+                }
+            }
+        }
+
+        return -1;
+    }
+
+    private static int GetIndexOfLastFileEntryShellItemEventOfSameTime_ChainOfResponsibility(List<EventViewModel> events, int startIndex)
+    {
+        int lastIndex = FindLastIndexOfSameTimeAndSourceType_ChainOfResponsibility(events, startIndex);
+        int output = -1;
+
+        while (startIndex <= lastIndex)
+        {
+            bool isSearchedFormat = events[startIndex].Format == "lnk/shell_items" || events[startIndex].Format == "filestat";
+            if (isSearchedFormat)
+            {
+                output = startIndex;
+            }
+
+            startIndex++;
+        }
+
+        return output == -1 ? lastIndex : output;
+    }
+
+    private static int FindLastIndexOfSameTimeAndSourceType_ChainOfResponsibility(List<EventViewModel> events, int startIndex)
+    {
+        while (startIndex < events.Count - 1 && events[startIndex].Source == "FILE" && events[startIndex + 1].Source == "FILE")
+        {
+            bool isSearchedSourceType = events[startIndex].SourceType == "File entry shell item";
+            bool isSameDate = events[startIndex].FullDate.CompareTo(events[startIndex + 1].FullDate) == 0;
+
+            if (isSearchedSourceType && isSameDate)
+            {
+                startIndex++;
+            }
+            else
+            {
+                break;
+            }
+        }
+
+        return startIndex;
+    }
+
+    private int GetIndexOfLastFileEventOfSameTime_ChainOfResponsibility(List<EventViewModel> events, int startIndex)
+    {
+        int lastValidIndex = startIndex;
+
+        while (startIndex < events.Count - 1 && events[startIndex].Source == "FILE" && events[startIndex + 1].Source == "FILE")
+        {
+            if (_lowLevelAbstractorUtils.IsValidFileEvent(events[startIndex]) && events[startIndex].FullDate.CompareTo(events[startIndex + 1].FullDate) != 0)
+            {
+                return startIndex;
+            }
+
+            if (_lowLevelAbstractorUtils.IsValidFileEvent(events[startIndex]))
+            {
+                lastValidIndex = startIndex;
+            }
+
+            startIndex++;
+        }
+
+        return lastValidIndex;
+    }
+
+    private static bool AreOlecfEventsOfSameTime_ChainOfResponsibility(EventViewModel firstEvent, EventViewModel secondEvent)
+    {
+        bool isSameSource = firstEvent.Source == secondEvent.Source;
+        bool isSameTime = firstEvent.FullDate.CompareTo(secondEvent.FullDate) == 0;
+
+        return isSameSource && isSameTime;
     }
 }
