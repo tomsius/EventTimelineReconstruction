c867e0a196df9f3344c16ac1c4e0640fe5a0e1be
Tomas Kašelynas <tomaskas98@gmail.com>
2023-05-02 11:24:39 +0300

    Implement

--- EventTimelineReconstruction/EventTimelineReconstruction/Services/L2tCSVEventsImporter.cs
+++ EventTimelineReconstruction/EventTimelineReconstruction/Services/L2tCSVEventsImporter.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Runtime.InteropServices;
 using EventTimelineReconstruction.Models;
 
 namespace EventTimelineReconstruction.Services;
@@ -12,25 +13,24 @@ public sealed class L2tCSVEventsImporter : IEventsImporter
 
     public List<EventModel> Import(string path, DateTime fromDate, DateTime toDate)
     {
-        IEnumerable<string> rows = File.ReadLines(path).Skip(1);
-        List<EventModel> events = new();
+        List<string> rows = File.ReadLines(path).Skip(1).ToList();
+        List<EventModel> events = new(rows.Count);
 
-        int lineNumber = 2;
-
-        foreach (string line in rows)
+        Span<string> rowsAsSpan = CollectionsMarshal.AsSpan(rows);
+        for (int i = 0; i < rowsAsSpan.Length; i++)
         {
-            string[] columns = line.Split(',');
+            string[] columns = rowsAsSpan[i].Split(',');
 
             if (columns.Length != _colCount)
             {
-                lineNumber++;
                 continue;
             }
 
             try
             {
-                EventModel eventModel = ConvertRowToModel(columns, lineNumber);
-                DateTime eventDate = new(eventModel.Date.Year, eventModel.Date.Month, eventModel.Date.Day, eventModel.Time.Hour, eventModel.Time.Minute, eventModel.Time.Second);
+                EventModel eventModel = ConvertRowToModel(columns, i + 2);
+                DateTime eventDate = new(eventModel.Date.Year, eventModel.Date.Month, eventModel.Date.Day,
+                                         eventModel.Time.Hour, eventModel.Time.Minute, eventModel.Time.Second);
 
                 if (DateTime.Compare(eventDate, fromDate) >= 0 && DateTime.Compare(eventDate, toDate) <= 0)
                 {
@@ -44,10 +44,6 @@ public sealed class L2tCSVEventsImporter : IEventsImporter
             catch (IndexOutOfRangeException)
             {
                 continue;
-            }
-            finally
-            {
-                lineNumber++;
             }
         }
 
