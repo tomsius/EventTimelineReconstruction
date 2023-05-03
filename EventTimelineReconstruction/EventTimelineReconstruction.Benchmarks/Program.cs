ac214ea820278a044e215fc286fa0a5355d4e70d
Tomas Kašelynas <tomaskas98@gmail.com>
2023-05-03 09:31:33 +0300

    Add all benchmarks to run

--- EventTimelineReconstruction/EventTimelineReconstruction.Benchmarks/Program.cs
+++ EventTimelineReconstruction/EventTimelineReconstruction.Benchmarks/Program.cs
@@ -1,10 +1,10 @@
 ﻿using BenchmarkDotNet.Running;
 using EventTimelineReconstruction.Benchmarks;
 
-//BenchmarkRunner.Run<EventsImporterBenchmarks>();
-//BenchmarkRunner.Run<WorkSaverBenchmarks>();
-//BenchmarkRunner.Run<WorkLoaderBenchmarks>();
-//BenchmarkRunner.Run<HighLevelEventsBenchmarks>();
-//BenchmarkRunner.Run<LowLevelEventsBenchmarks>();
-//BenchmarkRunner.Run<HighLevelArtefactsBenchmarks>();
-//BenchmarkRunner.Run<LowLevelArtefactsBenchmarks>();
+BenchmarkRunner.Run<EventsImporterBenchmarks>();
+BenchmarkRunner.Run<WorkSaverBenchmarks>();
+BenchmarkRunner.Run<WorkLoaderBenchmarks>();
+BenchmarkRunner.Run<HighLevelEventsBenchmarks>();
+BenchmarkRunner.Run<LowLevelEventsBenchmarks>();
+BenchmarkRunner.Run<HighLevelArtefactsBenchmarks>();
+BenchmarkRunner.Run<LowLevelArtefactsBenchmarks>();
